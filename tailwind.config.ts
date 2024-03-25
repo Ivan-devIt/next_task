aed472d275b48acce17c6311bec98b0fb7d49ee4
ivan.oliinyk <ivan.oleinik@devit.group>
2024-03-25 09:28:59 +0100

    update tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,18 +26,19 @@ const config: Config = {
         '5xl': '2560px'
       },
 
-      mm: ['0.875rem', '1.25rem'], // (size:14px line-height:20px)
-      sm: ['0.9375rem', '1.5rem'], // (size:15px line-height:24px)
-      ml: ['1rem', '1.5rem'], // (size:16px line-height:24px)
-      md: ['1.125rem', '1.6875rem'], // (size:18px line-height:27px)
-      xl: ['1.5rem', '2.25rem'], // (size:24px line-height:36px)
-      '1xl': ['2.5rem', '3.75rem'], // (size:40px line-height:60px)
-      '2xl': ['2rem', '3rem'], // (size:32px line-height:48px)
-      '3xl': ['1.5rem', '2.25rem'], // (size:24px line-height:36px)
-      '4xl': ['1.125rem', '1.688rem'], // (size:18px line-height:27px)
-      '5xl': ['2rem', '3rem'], // (size:32px line-height:48px)
-      '6xl': ['1.1rem', '1.6rem'], // (size:17px line-height:25px)
-    },
+      fontSize: {
+        mm: ['0.875rem', '1.25rem'], // (size:14px line-height:20px)
+        sm: ['0.9375rem', '1.5rem'], // (size:15px line-height:24px)
+        ml: ['1rem', '1.5rem'], // (size:16px line-height:24px)
+        md: ['1.125rem', '1.6875rem'], // (size:18px line-height:27px)
+        xl: ['1.5rem', '2.25rem'], // (size:24px line-height:36px)
+        '1xl': ['2.5rem', '3.75rem'], // (size:40px line-height:60px)
+        '2xl': ['2rem', '3rem'], // (size:32px line-height:48px)
+        '3xl': ['1.5rem', '2.25rem'], // (size:24px line-height:36px)
+        '4xl': ['1.125rem', '1.688rem'], // (size:18px line-height:27px)
+        '5xl': ['2rem', '3rem'], // (size:32px line-height:48px)
+        '6xl': ['1.1rem', '1.6rem'] // (size:17px line-height:25px)
+      },
 
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
