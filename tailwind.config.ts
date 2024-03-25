f8f00fb1d756837fb62f99f507ee4d6b672f4651
ivan.oliinyk <ivan.oleinik@devit.group>
2024-03-25 09:25:17 +0100

    update-tailwind-config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        sl: '320px',
+        ss: '360px',
+        ml: '428px',
+        ms: '480px',
+        mm: '576px',
+        sm: '680px',
+        md: '768px',
+        lm: '992px',
+        lg: '1024px',
+        xl: '1280px',
+        '1xl': '1366px',
+        '3xl': '1760px',
+        '4xl': '1920px',
+        '5xl': '2560px'
+      },
+
+      mm: ['0.875rem', '1.25rem'], // (size:14px line-height:20px)
+      sm: ['0.9375rem', '1.5rem'], // (size:15px line-height:24px)
+      ml: ['1rem', '1.5rem'], // (size:16px line-height:24px)
+      md: ['1.125rem', '1.6875rem'], // (size:18px line-height:27px)
+      xl: ['1.5rem', '2.25rem'], // (size:24px line-height:36px)
+      '1xl': ['2.5rem', '3.75rem'], // (size:40px line-height:60px)
+      '2xl': ['2rem', '3rem'], // (size:32px line-height:48px)
+      '3xl': ['1.5rem', '2.25rem'], // (size:24px line-height:36px)
+      '4xl': ['1.125rem', '1.688rem'], // (size:18px line-height:27px)
+      '5xl': ['2rem', '3rem'], // (size:32px line-height:48px)
+      '6xl': ['1.1rem', '1.6rem'], // (size:17px line-height:25px)
+    },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -22,15 +52,21 @@ const config: Config = {
         xl: '4px 4px 8px rgba(0, 0, 0, 0.5)'
       },
 
-      screens: {
-        mm: '576px',
-        '3xl': '1760px',
-        '4xl': '1920px',
-        '5xl': '2560px'
-      },
-
       gridTemplateRows: {
         layout: 'auto 1fr auto'
+      },
+
+      colors: {
+        primary: '#42ABDE',
+        primaryDark: '#2194CC',
+        secondaryDark: '#231F20',
+        gray: '#808080',
+        black: '#231F20',
+        lightGray: '#F8F8F8',
+        gainsboro: '#DDDDDD',
+        rose: '#E35F5E',
+        green: '#5BB984',
+        whitePrimary: '#ffffff'
       }
     }
   },
